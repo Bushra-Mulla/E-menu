adc4cab90f9d4643c87e6136d9f8a85a4c8ee33f
Bushra-Mulla <bushra.1416@hotmail.com>
2021-09-08 17:58:39 +0300

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,22 +10,51 @@ $(".search-btn").click(function () {
 });
 
 
-const phoneInputField = document.querySelector("#phone");
-const phoneInput = window.intlTelInput(phoneInputField, {
-  preferredCountries: ["sa"],
-  separateDialCode: true,
-  autoFormat: true,
-  utilsScript:
-    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
-});
 
-function show_toast(index,e) { 
-$(document).on("click", "i", function (e) {
-  e.preventDefault();
-});
-  $(`.toast#${index}`).toast("show");
-} 
+$(document).ready(function() {
+   $(".touchspin").TouchSpin({
+     buttondown_class: "btn btn-link text-decoration-none text-dark",
+     buttonup_class: "btn btn-link text-decoration-none text-dark",
+   });
+   $(".modal").on("hidden.bs.modal", function (e) {
+     if ($(".modal:visible").length) {
+       $("body").addClass("modal-open");
+     }
+   });
 
+  var inputContainer = document.querySelector(".auth-input-wrapper");
+  inputContainer.onkeyup = function (e) {
+    var n = e.srcElement,
+      a = parseInt(n.attributes.maxlength.value, 10),
+      t = n.value.length;
+    if (8 === e.keyCode) {
+      if (0 === t)
+        for (var r = n; (r = r.previousElementSibling) && null != r; )
+          if ("input" == r.tagName.toLowerCase()) {
+            r.focus();
+            break;
+          }
+    } else if (t >= a)
+      for (r = n; (r = r.nextElementSibling) && null != r; )
+        if ("input" == r.tagName.toLowerCase()) {
+          r.focus();
+          break;
+        }
+  };
+
+    $("input[name='method']").click(function () {
+      $("input[name='method']").next().removeClass("active");
+      $(this).next().addClass("active");
+      if ($(this).next().hasClass("phone-method")) {
+        $(".phone-section").removeClass("d-none");
+        $(".email-section").addClass("d-none");
+      } else {
+        $(".phone-section").addClass("d-none");
+        $(".email-section").removeClass("d-none");
+      }
+    });
+        
+})
 
 function reset_success() {
   Swal.fire({
@@ -41,4 +70,69 @@ function reset_success() {
     buttonsStyling: false,
   });
 }
+
+function show_toast(index, e) {
+  $(document).on("click", "i", function (e) {
+    e.preventDefault();
+  });
+  $(`.toast#${index}`).toast("show");
+} 
+
+$(function () {console.log('object')
+        $(".slider4").slick({
+          infinite: true,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          nextArrow:
+            '<button class="carousel-control-next border-0"> <span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></button>',
+          prevArrow:
+            '<button class="carousel-control-prev border-0"> <span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></button>',
+          responsive: responsive_slide(4),
+          
+        });
+        $(".slider3").slick({
+          infinite: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          nextArrow:
+            '<button class="carousel-control-next border-0"> <span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></button>',
+          prevArrow:
+            '<button class="carousel-control-prev border-0"> <span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></button>',
+          responsive: responsive_slide(3),
+        });
+    });
+    function responsive_slide(slideNumber){
+      return [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: slideNumber-1,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ];
+    }
+    
+    const phoneInputField = document.querySelector("#phone");
+    const phoneInput = window.intlTelInput(phoneInputField, {
+      preferredCountries: ["sa"],
+      separateDialCode: true,
+      autoFormat: true,
+      utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+    });
 
